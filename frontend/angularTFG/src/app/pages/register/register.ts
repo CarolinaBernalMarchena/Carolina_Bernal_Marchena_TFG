f43dbd333f19dabc8399b7009fdadd173ef2e188
carolinabm2000 <carolinabm2000@gmail.com>
2026-05-25 19:15:36 +0200

    cambio rutas en register

--- frontend/angularTFG/src/app/pages/register/register.ts
+++ frontend/angularTFG/src/app/pages/register/register.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  Validators,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth';
 
@@ -11,39 +16,34 @@ import { AuthService } from '../../services/auth';
   styleUrl: './register.scss',
 })
 export class Register {
-
   registerForm: FormGroup;
 
   constructor(
     private fb: FormBuilder,
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
   ) {
-
     this.registerForm = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      password: ['', [Validators.required, Validators.minLength(6)]],
     });
-
   }
 
   onSubmit() {
-
     if (this.registerForm.invalid) {
       return;
     }
 
     const formData = {
       ...this.registerForm.value,
-      role: 'user'
+      role: 'user',
     };
 
     this.authService.register(formData).subscribe({
       next: (res: any) => {
-
         console.log('Usuario creado correctamente');
-        
+
         if (res.token) {
           this.authService.setToken(res.token);
         }
@@ -53,13 +53,11 @@ export class Register {
       error: (err) => {
         console.error('Error al registrar usuario', err);
         alert('Este email ya está en uso');
-      }
+      },
     });
-
   }
 
-  navigateToHome() {
-    this.router.navigate(['/home']);
+  navigateToLogin() {
+    this.router.navigate(['/login']);
   }
-
 }
